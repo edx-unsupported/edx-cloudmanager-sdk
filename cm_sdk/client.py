9918af254918231cade52ab5130002639504d315
Edward Zarecor <ed@edx.org>
2015-10-07 12:52:31 -0400

    Up for intial review

--- cm_sdk/client.py
+++ cm_sdk/client.py
@@ -1,29 +1,60 @@
-import json
-from json import JSONEncoder
 import requests
+from cm_sdk.api import APIException
 
 class CloudManagerClient(object):
-    def __init__(self, base_url, group_id, api_user, api_key):
-        self.base_url = base_url + "/api/public/v1.0"
+    """
+    Simple client for interacting over HTTP with the the MongoDB
+    CloudManager API.
+    """
+
+    HEADERS = {'Content-type': 'application/json'}
+
+    def __init__(self, api_host, group_id, api_user, api_key):
+        self.base_url = api_host + "/api/public/v1.0"
         self.group_id = group_id
         self.http_digest_auth = requests.auth.HTTPDigestAuth(api_user, api_key)
 
-    def get(self, path):
-        api_target = self.base_url + path
-        r = requests.get(api_target, auth=self.http_digest_auth)
-        return r.json()
 
-    def put():
-        # TODO: implement me
-        pass
+    def _get_final_url(self, url_template):
+        return self.base_url + url_template.format(group_id=self.group_id)
+
+
+    def _valid_response(self, response):
+        if 'error' in response:
+            raise APIException(response['error'])
+        return response
+
+
+    #
+    # HTTP Request methods
+    #
+    def get(self, path):
+        r = requests.get(self._get_final_url(path), auth=self.http_digest_auth)
+        return self._valid_response(r.json())
+
+
+    def put(self, path, data):
+        # Not using the requests json keyword which expects a python dict
+        # intentionally.
+        r = requests.post(self._get_final_url(path),
+                          auth=self.http_digest_auth,
+                          headers=self.HEADERS,
+                          data=data.to_json())
+        return self._valid_response(r.json())
+
 
     def post(self, path, data):
-        api_target = self.base_url + path
-        return json.dumps(data, cls=MyEncoder)
-        # TODO WIP
-        #r = requests.post(api_target, auth=self.http_digest_auth, data=data)
-        
-    def delete():
-        # TODO: implement me
-        pass
+        # Not using the requests json keyword which expects a python dict
+        # intentionally.
+        r = requests.post(self._get_final_url(path),
+                          auth=self.http_digest_auth,
+                          headers=self.HEADERS,
+                          data=data.to_json())
+        return self._valid_response(r.json())
 
+
+    def delete(self, path):
+        r = requests.delete(self._get_final_url(path),
+                            auth=self.http_digest_auth,
+                            headers=self.HEADERS)
+        return self._valid_response(r.json())
--- cm_sdk/models/__init__.py
+++ cm_sdk/models/__init__.py
@@ -1,10 +1,110 @@
+import json
+from json import JSONEncoder
+from enum import Enum
+
 __author__ = 'e0d'
 
-class CommonEqualityMixin(object):
 
+class CloudManagerJSONEncoder(JSONEncoder):
+    """
+    A custom JSON encoder to provide transparent conversion between
+    CloudManager JSON format and the Python classes that represent API
+    objects in the SDK.
+    """
+    # credit
+    # http://stackoverflow.com/questions/19053707/convert-snake-case-snake-case-to-lower-camel-case-lowercamelcase-in-python
+    def to_camel_case(self, snake_str):
+        # Deal with the case were @property creates class property 
+        # keys with an initial '_'
+        snake_str = snake_str.lstrip('_')
+        # Split remaining name on '_'
+        components = snake_str.split('_')
+        return components[0].lower() + "".join(x.title() for x in components[1:])
+
+    def default(self, o):  # pylint: disable=method-hidden
+        if isinstance(o, CloudManagerEnum):
+            return o.to_json()
+        elif hasattr(o, "__dict__"):
+            # converts class parameter names from snake case to camel case expected
+            # by the API and prunes nulls
+            return dict((self.to_camel_case(k), v) for k, v in o.__dict__.iteritems() if v)
+        else:
+            return None
+
+
+
+class CommonEqualityMixin(object):
+    """
+    Mixin class to provide deep eqality as equivalence for API objects 
+    """
     def __eq__(self, other):
-        return (isinstance(other, self.__class__)
-            and self.__dict__ == other.__dict__)
+
+        #
+        # must be an instances of the same class
+        #
+        if not isinstance(other, self.__class__):
+            return False
+
+        # expectation that keys are the same breaks for entities
+        # returned from the API
+        my_keys, other_keys = (sorted(self.__dict__.keys()), sorted(other.__dict__.keys()))
+
+        if not my_keys == other_keys:
+            return False
+
+        for k in my_keys:
+
+            v0 = self.__dict__[k]
+            v1 = other.__dict__[k]
+
+            if v0 is not None:
+                if isinstance(v0, list):
+
+                    # Short circuit if the lists are not equal in length
+                    if not len(v0) == len(v1):
+                        return False
+
+                    # Compare each item in the list
+                    for item0, item1 in zip(v0, v1):
+                        if not item0 == item1:
+                            return False
+                else:
+                    if not v0 == v1:
+                        return False
+        return True
+
 
     def __ne__(self, other):
         return not self.__eq__(other)
+
+class CloudManagerBase(CommonEqualityMixin):
+    """
+    Base class for python representations of API objects that provides
+    utility methods for static initialization.
+    """
+    @classmethod
+    def from_json(cls, json_in):
+
+        _dict = json.loads(json_in)
+        return cls.from_dict(_dict)
+
+    @classmethod
+    def from_dict(cls, dict_in):
+
+        instance = cls()
+
+        for k, v in dict_in.items():
+            setattr(instance, k, v)
+        return instance
+
+    def to_json(self):
+        return json.dumps(self, cls=CloudManagerJSONEncoder)
+
+class CloudManagerEnum(Enum):
+
+    def to_json(self):
+        """
+        Return the value of the current instance of the Enum for JSON
+        serialization.
+        """
+        return str(self.value)
