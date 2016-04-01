f1895c6438dbe8a320832b5e358ea9e1cfcf2c5c
Edward Zarecor <ed@edx.org>
2016-04-01 15:12:29 -0400

    truthy bug fix

--- cm_sdk/models/common/common.py
+++ cm_sdk/models/common/common.py
@@ -20,7 +20,7 @@ class CloudManagerJSONEncoder(JSONEncoder):
         elif hasattr(o, "state"):
             # converts class parameter names from snake case to camel case expected
             # by the API and prunes nulls
-            return dict((to_camel_case(k), v) for k, v in o.state.iteritems() if v)
+            return dict((to_camel_case(k), v) for k, v in o.state.iteritems() if v is not None)
         else:
             return None
 
